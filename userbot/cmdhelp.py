67e12925621c68f4ef909cb5b44b2f986a0d3010
SakirBey1 <86895448+SakirBey1@users.noreply.github.com>
2021-07-13 21:17:05 +0300

    Update cmdhelp.py

--- userbot/cmdhelp.py
+++ userbot/cmdhelp.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2020 Yusuf Usta.
+# Copyright (C) 2020 SakirBey.
 #
 # Licensed under the GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 
-# SiriUserBot - Berceste
+# SakirUserBot - SakirBey
 
 from userbot import PATTERNS, CMD_HELP, CMD_HELP_BOT
 
