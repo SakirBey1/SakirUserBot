d8b2077aef42762df574547cb1318583c804c07f
SakirBey1 <86895448+SakirBey1@users.noreply.github.com>
2021-07-13 22:17:14 +0300

    Update anti_spambot.py

--- userbot/modules/anti_spambot.py
+++ userbot/modules/anti_spambot.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# SiriUserBot - Berceste
+# SakirUserBot - SakirBey 
 #
 
 ''' Gruba katılan spamcıları banlamada yardımcı olan modüldür. '''
