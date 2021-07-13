e67d9adfe9fe63481f48af10a4e260807d042988
SakirBey1 <86895448+SakirBey1@users.noreply.github.com>
2021-07-13 22:16:02 +0300

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -3,7 +3,7 @@
 # Licensed under the GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 ##
-# - tg: @siriot - @sirisupport - Berceste  # Thx: fireganqQ
+# SakirUserBot - SakirBey 
 
 
 """ AFK ile ilgili komutları içeren UserBot modülü """
