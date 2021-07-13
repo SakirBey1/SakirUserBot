f9434c3c9286cee7d2444b837d524f9a57ecec47
SakirBey1 <86895448+SakirBey1@users.noreply.github.com>
2021-07-13 23:09:33 +0300

    Update liste.py

--- userbot/modules/liste.py
+++ userbot/modules/liste.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2020 Yusuf Usta.
+# Copyright (C) 2020 SakirBey.
 #
 # Licensed under the GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 #
 
-# SiriUserBot - ErdemBey - Midy
+# SakirUserBot - SakirBey
 
 from userbot import CMD_HELP
 from userbot.events import register
