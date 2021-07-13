e4bd91a3bbe589c62cbd2fa8ee31c3721170ac80
SakirBey1 <86895448+SakirBey1@users.noreply.github.com>
2021-07-13 21:14:09 +0300

    Update language.py

--- userbot/language.py
+++ userbot/language.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2020 Yusuf Usta.
+# Copyright (C) 2020 SakirBey.
 #
 # Licensed under the GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 #
 
-# SiriUserBot - Berceste
+# SakirUserBot - SakirBey
 
 from . import LANGUAGE, LOGS, bot, PLUGIN_CHANNEL_ID
 from json import loads, JSONDecodeError
