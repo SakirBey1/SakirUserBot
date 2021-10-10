9377d2401d471250073e704531b586b4709e131d
SakirBey1 <sakirhack81@gmail.com>
2021-10-10 20:18:10 +0300

    Update GenerateStringSession.py

--- GenerateStringSession.py
+++ GenerateStringSession.py
@@ -37,7 +37,7 @@ class InteractiveTelegramClient(TelegramClient):
             proxy=proxy
         )
         self.found_media = {}
-        print('@SakirUserBot1 String Alıcıya Hoş Geldiniz')
+        print('@UserBots0 String Alıcıya Hoş Geldiniz')
         print('[i] Telegramın Sunucularına Bağlanılıyor...')
         try:
             loop.run_until_complete(self.connect())
@@ -78,7 +78,7 @@ class InteractiveTelegramClient(TelegramClient):
 
 
 if __name__ == '__main__':
-   print("[i] sakir String V3\n@SakirUserBot1\n\n")
+   print("[i] sakir String V3\n@UserBots0\n\n")
    print("[1] OtoMatik API ID/HASH Alıcı")
    print("[2] String Alıcı\n")
    
